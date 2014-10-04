5e49a3f649a0365bef1ff9842e22af6f7a7a6838
Hriday Kemburu <hridaykemburu@Hridays-MacBook-Pro.local>
2014-10-04 15:42:12 -0700

    working on table cells

---
+++ Pique/Pique/LocationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationTableViewController.h
+//  Pique
+//
+//  Created by Hriday Kemburu on 10/4/14.
+//  Copyright (c) 2014 Hriday Kemburu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationTableViewController : UITableViewController
+
+@end
